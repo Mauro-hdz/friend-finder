5c0ce05d0103c9a802d49b266a4f8c56f24c3ccf
unknown <mauro.hdz99@gmail.com>
2019-05-24 19:55:34 -0500

    completed full final html

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+// middleware handler?
+const bodyParser = require("body-parser");
 // Adding express
 const express = require("express");
+
+const app = express();
+
 // Adding path
 const path = require("path");
 
-const app = express();
-const PORT = process.env || 8080;
+// creating a port
+const PORT = process.env || 3000;
 
 app.get('/', function (req, res) {
     res.send('Whats popping?')
-  })
+  });
    
-  app.listen(3000)
+
+  // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// // parse application/json
+// app.use(bodyParser.json())
+ 
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
+
+// app.listen(PORT, function(){
+//     console.log("App listening on port: "+ PORT);
+// });
 
