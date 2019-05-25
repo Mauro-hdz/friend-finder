6141bb2e7ac19419c79a73ae54e75acbcd910eff
unknown <mauro.hdz99@gmail.com>
2019-05-25 01:34:46 -0500

    completed routing for each webpage

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+
+// Not sure about this yet, just an idea
+// function newPerson(name, favFood, answers) {
+//     this.name = name,
+//     this.favFood = favFood,
+//     this.answers = answers
+// };
+
+// var someone = new newPerson(Bobby, Pizza, [1,2,3,4,5,1,2,3,4,5]);
+
+let peopleArray =[
+    {
+        "name":"Ahmed",
+  "favoriteFood":"Tacos",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+    },
+]
+module.exports = peopleArray;
--- server.js
+++ server.js
@@ -2,18 +2,16 @@
 const bodyParser = require("body-parser");
 // Adding express
 const express = require("express");
+// Adding path
+const path = require("path");
 
 const app = express();
 
-// Adding path
-// const path = require("path");
+
 
 // creating a port
 const PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-    res.send('Hellooo random person')
-  });
    
 
  // parse application/x-www-form-urlencoded
@@ -22,11 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require("./app/routing/apiRoutes") (app);
+
+require("./app/routing/htmlRoutes") (app);
 
 
 app.listen(PORT, function() {
