1cea3e9810b68a75b3c7f80fb0a5d803b9695973
unknown <mauro.hdz99@gmail.com>
2019-05-24 23:57:40 -0500

    created starter code for server.js

--- server.js
+++ server.js
@@ -6,29 +6,31 @@ const express = require("express");
 const app = express();
 
 // Adding path
-const path = require("path");
+// const path = require("path");
 
 // creating a port
-const PORT = process.env || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.send('Whats popping?')
+    res.send('Hellooo random person')
   });
    
 
-  // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
+ // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
  
-// // parse application/json
-// app.use(bodyParser.json())
+// parse application/json
+app.use(bodyParser.json())
  
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// });
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
-// app.listen(PORT, function(){
-//     console.log("App listening on port: "+ PORT);
-// });
+
+app.listen(PORT, function() {
+  console.log("connected port: " +PORT);
+});
+
 
